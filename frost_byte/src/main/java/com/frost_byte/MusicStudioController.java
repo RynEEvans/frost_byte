5cd782afd2b55334ff0161c2ae68b2cfc1b5aa1c
Ryan Evans <112006148+RynEEvans@users.noreply.github.com>
2025-04-25 16:14:24 -0400

    catching up

---
+++ frost_byte/src/main/java/com/frost_byte/MusicStudioController.java
@@ -0,0 +1,11 @@
+package com.frost_byte;
+
+public class MusicStudioController {
+    
+    private PrimaryController primary;
+
+    public void setPrimaryController(PrimaryController controller) {
+        this.primary = controller;
+    }
+    
+}
---
+++ frost_byte/src/main/java/com/frost_byte/SongPlayerController.java
@@ -0,0 +1,11 @@
+package com.frost_byte;
+
+public class SongPlayerController {
+    
+    private PrimaryController primary;
+
+    public void setPrimaryController(PrimaryController controller) {
+        this.primary = controller;
+    }
+    
+}
