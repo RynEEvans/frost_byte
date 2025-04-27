9ae65014e7c0b32a96a397d19f43b4b7b6eeeffe
Ryan Evans <112006148+RynEEvans@users.noreply.github.com>
2025-04-27 00:48:26 -0400

    catching up

---
+++ frost_byte/src/main/java/com/frost_byte/PlaylistController.java
@@ -0,0 +1,11 @@
+package com.frost_byte;
+
+public class PlaylistController {
+    
+    private PrimaryController primary;
+
+    public void setPrimaryController(PrimaryController controller) {
+        this.primary = controller;
+    }
+    
+}
